77bfa4bfdd2c23007b3de757831fb35df75bea4e
Tomoe-Yoko <osmanthus31172@gmail.com>
2024-08-05 22:22:46 +0900

    削除ボタンの追加

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -9,6 +9,11 @@ const Todo = () => {
     setTodoList([...todoList, { text: inputText }]);
     setInputText("");
   };
+
+  const remove = (index) => {
+    setTodoList(todoList.filter((_, i) => i !== index));
+  };
+
   return (
     <div className="App">
       <div>
@@ -29,13 +34,19 @@ const Todo = () => {
           </button>
         </div>
         <div className="todo-container">
-          <ol id="todos" className="todos">
+          <ol>
             {todoList.map((todo, index) => (
-              <li
-                key={index}
-                className="w-3/4 mx-auto my-4 p-4 text-xl bg-blue-300 list-decimal rounded-xl"
-              >
-                {todo.text}
+              <li key={index} className="flex justify-center mx-auto w-3/4">
+                <div className="w-3/4  my-4 p-4 text-xl bg-blue-300 list-decimal rounded-xl">
+                  {todo.text}
+                </div>
+                <button
+                  onClick={() => remove(index)}
+                  className="p-4 bg-gray-300
+                   text-black m-4 rounded-xl"
+                >
+                  削除
+                </button>
               </li>
             ))}
           </ol>
